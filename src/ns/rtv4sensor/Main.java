58e589e9c02e9e4dc4780e0c28921bff15881a50
NaoyaSawaguchi(sr-pc) <pmsouth.2dx@gmail.com>
2016-05-29 20:52:12 +0900

    JFreeChartをテストした。

--- src/ns/rtv4sensor/Main.java
+++ src/ns/rtv4sensor/Main.java
@@ -1,9 +1,40 @@
 package ns.rtv4sensor;
 
-public class Main {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class Main extends JFrame{
+
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		JFrame mainFrame = new Main();
+		
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setBounds(10, 10, 500, 500);
+		mainFrame.setTitle("JFreeChart Test");
+		mainFrame.setVisible(true);
 	}
 
+	Main(){
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		JFreeChart chart =
+				ChartFactory.createLineChart("ColorSensor Reflect",
+											 "Reflect",
+											 "ms",
+											 dataset,
+											 PlotOrientation.HORIZONTAL,
+											 true,
+											 false,
+											 false);
+		ChartPanel cPanel = new ChartPanel(chart);
+		this.getContentPane().add(cPanel, BorderLayout.CENTER);
+	}
 }
