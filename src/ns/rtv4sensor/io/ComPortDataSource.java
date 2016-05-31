7d9c84d037c43de5601518e179e7cc698c2c2ee3
NaoyaSawaguchi(sr-pc) <pmsouth.2dx@gmail.com>
2016-06-01 06:56:47 +0900

    COMポートをデータソースとするDataSourceを作成。

---
+++ src/ns/rtv4sensor/io/ComPortDataSource.java
@@ -0,0 +1,17 @@
+package ns.rtv4sensor.io;
+
+public class ComPortDataSource extends DataSource {
+
+	@Override
+	public boolean start() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean close() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+}
