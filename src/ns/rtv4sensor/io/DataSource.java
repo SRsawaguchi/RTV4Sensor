7701b397b635aca9995ac3827b094737fd3814ef
NaoyaSawaguchi(sr-pc) <pmsouth.2dx@gmail.com>
2016-06-01 06:38:18 +0900

    コンストラクタを作成。 何かしらのデータ受け取り先が必要ということで、 DataReceiverの設定を強制化。。。 （dataReceiver==nullの時はデータを破棄するという処理も可能だが...)

--- src/ns/rtv4sensor/io/DataSource.java
+++ src/ns/rtv4sensor/io/DataSource.java
@@ -1,5 +1,9 @@
 package ns.rtv4sensor.io;
 
 public abstract class DataSource {
-
+	DataReceiver dataReceiver;
+	
+	public DataSource(DataReceiver dataReceiver) {
+		this.dataReceiver = dataReceiver;
+	}
 }
