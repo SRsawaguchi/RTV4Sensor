f436686c58666a434faf65a4b9475df8c1af2968
NaoyaSawaguchi(sr-pc) <pmsouth.2dx@gmail.com>
2016-06-01 06:40:18 +0900

    やっぱDataReceiverのセットは強制しないことにした。

--- src/ns/rtv4sensor/io/DataSource.java
+++ src/ns/rtv4sensor/io/DataSource.java
@@ -3,7 +3,18 @@ package ns.rtv4sensor.io;
 public abstract class DataSource {
 	DataReceiver dataReceiver;
 	
+	public DataSource(){
+	}
+	
 	public DataSource(DataReceiver dataReceiver) {
+		this.dataReceiver = dataReceiver;
+	}
+
+	public DataReceiver getDataReceiver() {
+		return dataReceiver;
+	}
+
+	public void setDataReceiver(DataReceiver dataReceiver) {
 		this.dataReceiver = dataReceiver;
 	}
 }
